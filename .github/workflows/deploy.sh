0db2bde8ca03ad87a9a854adf9aeaf405cecde29
Nick Watts <nwatts@broadinstitute.org>
2020-04-01 15:32:17 -0400

    Setup deploy preview links

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,6 +3,19 @@
 if [ "$GITHUB_EVENT_NAME" == "push" ]; then
     PATH_PREFIX="/blog" yarn run build
     gsutil -m rsync -r "public" "gs://gnomad-blog/master"
+
+    curl \
+      --request POST \
+      --url "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}" \
+      --header "authorization: Bearer ${GITHUB_TOKEN}" \
+      --header "content-type: application/json" \
+      --data '{
+        "state": "success",
+        "target_url": "https://gnomad.broadinstitute.org/blog",
+        "description": "Site deployed",
+        "context": "actions/deploy"
+      }'
+
 elif [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
     event_action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
     pr_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
@@ -12,5 +25,17 @@ elif [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
     else
         PATH_PREFIX="/blog/preview/${pr_number}" yarn run build
         gsutil -m rsync -r "public" "gs://gnomad-blog/pulls/${pr_number}"
+
+        curl \
+          --request POST \
+          --url "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}" \
+          --header "authorization: Bearer ${GITHUB_TOKEN}" \
+          --header "content-type: application/json" \
+          --data "{
+            \"state\": \"success\",
+            \"target_url\": \"https://gnomad.broadinstitute.org/blog/preview/${pr_number}\",
+            \"description\": \"Site deployed\",
+            \"context\": \"actions/deploy\"
+          }"
     fi
 fi
