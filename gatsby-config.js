4cef5517e8db9e7786154f8874de355d1125ecc2
Nick Watts <nwatts@broadinstitute.org>
2020-04-13 11:53:04 -0400

    Add RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,8 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || "/blog",
   siteMetadata: {
     title: "gnomAD blog",
-    siteUrl: "https://gnomad.broadinstitute.org",
+    description: "Announcements from the gnomAD project",
+    siteUrl: "https://gnomad.broadinstitute.org/blog",
   },
   plugins: [
     {
@@ -31,5 +32,57 @@ module.exports = {
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-netlify-cms",
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            output: "/rss.xml",
+            title: "gnomAD blog",
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+          },
+        ],
+      },
+    },
   ],
 };
