36cc245c26ab7e330481ebf1c57aa5421ec2d9ae
Nick Watts <nwatts@broadinstitute.org>
2020-09-28 14:46:13 -0400

    Add sidebar to home page
    Resolves #8

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,50 +9,67 @@ const BlogIndex = ({ data }) => {
 
   return (
     <Layout title="Home">
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug;
-        return (
-          <article className="article" key={node.fields.slug}>
-            <header className="article-header">
-              <h2 className="article-title">
-                <Link to={node.fields.slug}>{title}</Link>
-              </h2>
-            </header>
+      <div className="article-list">
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug;
+          return (
+            <article className="article" key={node.fields.slug}>
+              <header className="article-header">
+                <h2 className="article-title">
+                  <Link to={node.fields.slug}>{title}</Link>
+                </h2>
+              </header>
 
-            <div className="article-meta">
-              <span className="article-date">{node.frontmatter.date}</span>
+              <div className="article-meta">
+                <span className="article-date">{node.frontmatter.date}</span>
 
-              {node.frontmatter.categories && node.frontmatter.categories.length && (
-                <span className="article-categories">
-                  {" in "}
-                  {node.frontmatter.categories.reduce((acc, category) => [
-                    ...acc,
-                    acc.length && " / ",
-                    <span key={category} className="article-category">
-                      {category}
-                    </span>,
-                  ])}
-                </span>
-              )}
+                {node.frontmatter.categories && node.frontmatter.categories.length && (
+                  <span className="article-categories">
+                    {" in "}
+                    {node.frontmatter.categories.reduce((acc, category) => [
+                      ...acc,
+                      acc.length && " / ",
+                      <span key={category} className="article-category">
+                        {category}
+                      </span>,
+                    ])}
+                  </span>
+                )}
 
-              {node.frontmatter.authors && node.frontmatter.authors.length && (
-                <div className="article-authors">{node.frontmatter.authors.join(", ")}</div>
-              )}
-            </div>
+                {node.frontmatter.authors && node.frontmatter.authors.length && (
+                  <div className="article-authors">{node.frontmatter.authors.join(", ")}</div>
+                )}
+              </div>
 
-            <section
-              className="article-body article-excerpt"
-              dangerouslySetInnerHTML={{
-                __html: node.excerpt,
-              }}
-            />
+              <section
+                className="article-body article-excerpt"
+                dangerouslySetInnerHTML={{
+                  __html: node.excerpt,
+                }}
+              />
 
-            <footer className="article-footer">
-              <Link to={node.fields.slug}>Continue reading</Link>
-            </footer>
-          </article>
-        );
-      })}
+              <footer className="article-footer">
+                <Link to={node.fields.slug}>Continue reading</Link>
+              </footer>
+            </article>
+          );
+        })}
+
+        <aside className="sidebar">
+          <h3>Recent posts</h3>
+          <ol className="recent-posts-list">
+            {posts.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug;
+              return (
+                <li key={node.fields.slug}>
+                  <Link to={node.fields.slug}>{title}</Link>
+                  <div className="article-date">{node.frontmatter.date}</div>
+                </li>
+              );
+            })}
+          </ol>
+        </aside>
+      </div>
     </Layout>
   );
 };
