748b4742ac8e8e75556c737d351c34c80cb3a68c
Nick Watts <nwatts@broadinstitute.org>
2020-10-30 11:02:48 -0400

    Use order field on author and category pages

--- src/templates/author.js
+++ src/templates/author.js
@@ -31,7 +31,7 @@ export const pageQuery = graphql`
   query($author: String!) {
     allMarkdownRemark(
       filter: { frontmatter: { authors: { in: [$author] } } }
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: { fields: [frontmatter___date, frontmatter___order], order: DESC }
     ) {
       edges {
         node {
--- src/templates/category.js
+++ src/templates/category.js
@@ -30,7 +30,7 @@ export const pageQuery = graphql`
   query($category: String!) {
     allMarkdownRemark(
       filter: { frontmatter: { categories: { in: [$category] } } }
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: { fields: [frontmatter___date, frontmatter___order], order: DESC }
     ) {
       edges {
         node {
