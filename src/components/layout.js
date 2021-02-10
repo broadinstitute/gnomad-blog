4c83269f7417111630f44a65949f91100ec23dae
Nick Watts <nwatts@broadinstitute.org>
2021-02-10 17:39:23 -0500

    Add description meta tag

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,12 @@ const Layout = ({ children, title }) => {
         htmlAttributes={{ lang: "en" }}
         title={title}
         titleTemplate={`%s | ${site.siteMetadata.title}`}
-      />
+      >
+        <meta
+          name="description"
+          content="The Genome Aggregation Database (gnomAD) is a resource developed by an international coalition of investigators, with the goal of aggregating and harmonizing both exome and genome sequencing data from a wide variety of large-scale sequencing projects, and making summary data available for the wider scientific community."
+        />
+      </Helmet>
       <Header siteTitle={site.siteMetadata.title} />
       <main>{children}</main>
     </React.Fragment>
