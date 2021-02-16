67cf45889c81370025daedec7daa9e892b134248
Nick Watts <nwatts@broadinstitute.org>
2021-02-16 09:40:09 -0500

    Add RSS feed for changelog
    Resolves #25

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,6 +96,41 @@ const config = {
               }
             `,
           },
+          {
+            output: "/changelog.xml",
+            title: "gnomAD changelog",
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  filter: { fileAbsolutePath: { regex: "/changelog/" } }
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+          },
         ],
       },
     },
