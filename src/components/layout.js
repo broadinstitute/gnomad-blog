c20962a6c3fe1cfcbfb37ae58854a3178e50cf85
Riley Grant <grantrileyh@gmail.com>
2022-08-12 08:49:08 -0500

    Add banner to blog application

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,55 @@ import { useStaticQuery, graphql } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import Helmet from "react-helmet";
+import styled from "styled-components";
 
 import Header from "./header";
+
+const Link = styled.a`
+  color: #1173bb;
+  text-decoration: none;
+  &:visited,
+  &:active {
+    color: #1173bb;
+  }
+  &:focus,
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ExternalLink = styled(Link).attrs({
+  rel: "noopener noreferrer",
+  target: "_blank",
+})`
+  /* stylelint-ignore-line block-no-empty */
+`;
+
+const TopBarWrapper = styled.div`
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.23);
+
+  @media print {
+    display: none;
+  }
+`;
+
+const Banner = styled.div`
+  padding: 0.75em 0.5em;
+  background: rgb(17, 115, 187);
+  color: #fff;
+  text-align: center;
+
+  a {
+    color: #fff !important;
+  }
+`;
+
+const BANNER_CONTENT = (
+  <>
+    We&apos;re hiring! {" • "}
+    <ExternalLink href="https://broad.io/gnomad-cs">Computational scientist</ExternalLink>
+  </>
+);
 
 const Layout = ({ children, title }) => {
   const { site } = useStaticQuery(
@@ -30,7 +77,11 @@ const Layout = ({ children, title }) => {
           content="The Genome Aggregation Database (gnomAD) is a resource developed by an international coalition of investigators, with the goal of aggregating and harmonizing both exome and genome sequencing data from a wide variety of large-scale sequencing projects, and making summary data available for the wider scientific community."
         />
       </Helmet>
-      <Header siteTitle={site.siteMetadata.title} />
+      <TopBarWrapper>
+        <Header siteTitle={site.siteMetadata.title} />
+        {BANNER_CONTENT && <Banner>{BANNER_CONTENT}</Banner>}
+      </TopBarWrapper>
+
       <main>{children}</main>
     </React.Fragment>
   );
