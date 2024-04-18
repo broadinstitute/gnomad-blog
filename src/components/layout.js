372fb5296d8c02c838259165f7fdc5f464f28006
Riley Grant <grantrileyh@gmail.com>
2024-04-18 13:56:35 -0500

    feat(frontend): update banner for v4.1

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,16 +46,7 @@ const Banner = styled.div`
   }
 `;
 
-const BANNER_CONTENT = (
-  <>
-    We are aware of an issue in the gnomAD v4.0 exomes where well covered variants have lower than
-    expected allele numbers. This issue will be fixed in the upcoming v4.1 release. For more
-    information, see our write-up {/* @ts-expect-error */}
-    <ExternalLink href="https://docs.google.com/document/d/1Xm5ZIhmkh7hv2qEfCDS6J2T0IUZYiXP8pNClTlNvCGQ/edit?usp=sharing">
-      here.
-    </ExternalLink>{" "}
-  </>
-);
+const BANNER_CONTENT = <>gnomAD v4.1.0 is now available</>;
 
 const Layout = ({ children, title }) => {
   const { site } = useStaticQuery(
